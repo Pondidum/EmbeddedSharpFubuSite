e453c7ada6be61d0c5f32b99d69114bce6e4295d
Andy <andydavies2020@gmail.com>
2014-01-13 10:13:44 +0000

    Console: added ref to new service, added service registry

---
+++ EmbeddedSharpFubuSite/ServiceRegistry.cs
@@ -0,0 +1,23 @@
+﻿using FubuMVC.Diagnostics;
+using SomeServiceCore;
+using StructureMap.Configuration.DSL;
+
+namespace EmbeddedSharpFubuSite
+{
+    public class ServiceRegistry : Registry
+    {
+        public ServiceRegistry()
+        {
+            var p = new Processor();
+
+            For<IProcessor>().Singleton().Use(p);
+
+            //Scan(x =>
+            //{
+            //    x.TheCallingAssembly();
+            //    x.WithDefaultConventions();
+            //});
+
+        }
+    }
+}
